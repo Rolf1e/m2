0c36cd3b0236a3e79354f18326c1258f768e1672
Rolf1e <tigran.slama@laposte.net>
2021-10-22 14:32:02 +0200

    [Web Mobile] TP4 ex2 q3 flip hori

--- jee/TpImageTigran/app/src/main/java/com/example/tpimagetigran/MainActivity.java
+++ jee/TpImageTigran/app/src/main/java/com/example/tpimagetigran/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.tpimagetigran;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -10,7 +12,6 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.Objects;
@@ -31,6 +32,33 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(Intent.createChooser(loadImage, "Choose an image"), 1);
     }
 
+    public void flipImage(final View view) {
+        final int id = view.getId();
+        if (R.id.button_flip_hori == id) {
+            flipImageHori(findViewById(R.id.imageView));
+        } else if (R.id.button_flip_verti == id) {
+            flipImageVerti(findViewById(R.id.imageView));
+        }
+    }
+
+    private void flipImageHori(final ImageView image) {
+        final Bitmap bitmap = getBitmap(image);
+
+        final int width = bitmap.getWidth() - 1;
+        for (int x = 0; x < width / 2; x++) {
+            for (int y = 0; y < bitmap.getHeight() - 1; y++) {
+                final int pixel = bitmap.getPixel(x, y);
+                final int pixelOpposite = bitmap.getPixel(width - x, y);
+                bitmap.setPixel(x, y, pixelOpposite);
+                bitmap.setPixel(width - x, y, pixel);
+            }
+        }
+    }
+
+    private void flipImageVerti(final View viewById) {
+
+
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -56,5 +84,9 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
+    }
+
+    private static Bitmap getBitmap(final ImageView image) {
+        return ((BitmapDrawable) image.getDrawable()).getBitmap();
     }
 }
