07c639c8fa3ba73303d76c4110bdc72b30114dbb
Rolf1e <tigran.slama@laposte.net>
2021-10-22 10:19:52 +0200

    [Web Mobile] TP4 ex1 q2

--- webmobile/TestImplicite/app/src/main/java/com/example/testimplicite/MainActivity.java
+++ webmobile/TestImplicite/app/src/main/java/com/example/testimplicite/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.testimplicite;
 
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openingSms(final View view) {
+        System.out.println("Opening SMS !");
+    }
+
+    public void openingMms(final View view) {
+        System.out.println("Opening MMS !");
+    }
+
+    public void openingCalls(final View view) {
+        System.out.println("Opening calls !");
+    }
+
+    public void openingWeb(final View view) {
+        System.out.println("Opening web !");
+    }
+
+    public void openingMap(final View view) {
+        System.out.println("Opening Map !");
     }
 }
