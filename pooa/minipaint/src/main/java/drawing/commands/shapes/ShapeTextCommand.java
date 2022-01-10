ce5e3090bfcb4313336091c862bf7a9a120b77d3
Rolf1e <tigran.slama@laposte.net>
2022-01-10 14:27:59 +0100

    [POOA] TP7 - TP7 minor fix

--- pooa/minipaint/src/main/java/drawing/commands/shapes/ShapeTextCommand.java
+++ pooa/minipaint/src/main/java/drawing/commands/shapes/ShapeTextCommand.java
@@ -5,55 +5,54 @@ import drawing.panes.DrawingPane;
 import drawing.shapes.IShape;
 import drawing.shapes.decorators.TextDecorator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShapeTextCommand implements Command {
 
     private final DrawingPane pane;
     private final String text;
-
-    private IShape oldShape;
-    private IShape withText;
+    private final List<IShape> oldShapes;
+    private final List<IShape> shapesWithText;
 
     public static Command create(final DrawingPane pane,
                                  final String text) {
 
-        return new ShapeTextCommand(pane, text);
-    }
-
-    private ShapeTextCommand(final DrawingPane pane,
-                             final String text) {
-        this.pane = pane;
-        this.text = text;
+        return new ShapeTextCommand(pane, text, new ArrayList<>(), new ArrayList<>());
     }
 
     private ShapeTextCommand(final DrawingPane pane,
                              final String text,
-                             final IShape oldShape,
-                             final IShape withText) {
+                             final List<IShape> oldShapes,
+                             final List<IShape> shapesWithText) {
         this.pane = pane;
         this.text = text;
-        this.oldShape = oldShape;
-        this.withText = withText;
+        this.oldShapes = oldShapes;
+        this.shapesWithText = shapesWithText;
     }
 
     @Override
     public void execute() {
-        oldShape = pane.getSelectedShapes()
-                .get(0);
+        oldShapes.addAll(pane.getSelectedShapes());
 
-        pane.removeShape(oldShape);
-        withText = TextDecorator.create(oldShape, text);
-        pane.addShape(withText);
+        oldShapes.forEach(shape -> {
+            pane.removeShape(shape);
+            final var withText = TextDecorator.create(shape, text);
+            shapesWithText.add(withText);
+            pane.addShape(withText);
+        });
+
     }
 
     @Override
     public void undo() {
-        pane.removeShape(withText);
-        pane.addShape(oldShape);
+        shapesWithText.forEach(pane::removeShape);
+        oldShapes.forEach(pane::addShape);
     }
 
     @Override
     public Command duplicate() {
-        return new ShapeTextCommand(pane, text, oldShape, withText);
+        return new ShapeTextCommand(pane, text, oldShapes, shapesWithText);
     }
 
 }
