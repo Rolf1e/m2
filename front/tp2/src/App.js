89fde87d802c643132eb5640a7193effad3b48a9
Rolf1e <tigran.slama@laposte.net>
2022-02-28 22:03:40 +0100

    [Front - React] Editing todo tasks

--- front/tp2/src/App.js
+++ front/tp2/src/App.js
@@ -5,9 +5,18 @@ import FilterButton from "./components/FilterButton";
 import React, { useState } from "react";
 import { nanoid } from "nanoid";
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: task => !task.completed,
+  Completed: task => task.completed
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 function App(props) {
 
   const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState('All');
 
   function addTask(name) {
     const newTask = { id: "todo-" + nanoid(), name: name, completed: false };
@@ -32,6 +41,18 @@ function App(props) {
     setTasks(remainingTasks);
   }
 
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map(task => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        //
+        return { ...task, name: newName }
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
+
   const taskList = tasks.map(task => (
     <Todo
       id={task.id}
@@ -40,9 +61,13 @@ function App(props) {
       key={task.id}
       toggleTaskCompleted={toggleTaskCompleted}
       deleteTask={deleteTask}
+      editTask={editTask}
     />
-  )
-  );
+  ));
+
+  const filterList = FILTER_NAMES.map(name => (
+    <FilterButton key={name} name={name} />
+  ));
 
   // count task in list
   const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
