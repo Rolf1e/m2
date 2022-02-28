b5917e0d29ceee32037fc37211ba60d141bb906b
Rolf1e <tigran.slama@laposte.net>
2022-02-28 21:51:57 +0100

    [Front - React] Check box and delete tasks

--- front/tp2/src/App.js
+++ front/tp2/src/App.js
@@ -14,12 +14,32 @@ function App(props) {
     setTasks([...tasks, newTask]);
   }
 
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map(task => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        // use object spread to make a new object
+        // whose `completed` prop has been inverted
+        return { ...task, completed: !task.completed }
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter(task => id !== task.id);
+    setTasks(remainingTasks);
+  }
+
   const taskList = tasks.map(task => (
     <Todo
       id={task.id}
       name={task.name}
       completed={task.completed}
       key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
     />
   )
   );
